801a603591643c686c9b9c611e99521c258c39dd
Sona-Bhandari <125544499+Sona-Bhandari@users.noreply.github.com>
2023-03-12 21:13:40 -0700

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,24 @@
+package com.nextstep.Java.first;
+
+public class Function {
+	public void getValue() {
+		String name="sona";
+		int age=22;
+		displayValue(name,age);
+	}
+	public void displayValue(String a,int b) {
+		System.out.println(a);
+		System.out.println(b);
+	}
+	public static void main(String[]args) {
+		Function obj=new Function();
+				obj.getValue();
+				
+		
+				
+		
+				
+	}
+
+}
+
